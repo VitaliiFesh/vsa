1bc1a5632b223eb66819cee462199963f75a9cd0
Vitalii Feshchenko <feshchenko.vitalii@gmail.com>
2022-06-03 11:53:24 -0400

    myAllias and IO scheduler

---
+++ myAllias.sh
@@ -0,0 +1,8 @@
+alias vsa="systemctl status StarWindVSA"
+alias vsa_run_rescan="/opt/StarWind/StarWindVSA/drive_c/StarWind/hba_rescan.ps1"
+alias vsa_rescan="nano /opt/StarWind/StarWindVSA/drive_c/StarWind/hba_rescan.ps1"
+alias vsa_download="sudo wget https://ovf.starwind.com/tmplink/starwind-update.tar.gz --no-check-certificate; sudo tar -xf starwind-update.tar.gz; rm -f starwind-update.tar.gz"
+alias vsa_start="systemctl start StarWindVSA"
+alias vsa_stop="systemctl stop StarWindVSA"
+alias vsa_io="/opt/StarWind/StarWindVSA/drive_c/StarWind/script-io.sh"
+
---
+++ script-io.sh
@@ -0,0 +1,54 @@
+set -e
+echo
+lsblk
+echo
+read -p 'Choose a disk: ' disk
+cat /sys/block/$disk/queue/scheduler
+echo
+read -p 'Choose a scheduler: ' scheduler
+#echo $scheduler > /sys/block/$disk/queue/scheduler
+#echo "===========Changed=============="
+path='/etc/udev/rules.d/89-disk-scheduler.rules'
+
+if [ "$scheduler" == "noop" ]; then
+    
+    echo $scheduler > /sys/block/$disk/queue/scheduler    
+    cat /sys/block/$disk/queue/scheduler
+    sed -i -e '/^ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd\[a-z\]"/ s/^/#/;' $path
+    sed -i -e "/^ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\"/d" $path
+    sed -i "s/non-rotating disks/&\nACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\", ATTR{queue\/scheduler}=\"$scheduler\", ATTR{queue\/read_ahead_kb}=\"0\"/" $path 
+    
+elif [ "$scheduler" == "deadline" ]; then
+    
+    echo $scheduler > /sys/block/$disk/queue/scheduler
+    cat /sys/block/$disk/queue/scheduler
+    sed -i -e '/^ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd\[a-z\]"/ s/^/#/;' $path
+    sed -i -e "/^ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\"/d" $path
+    echo "ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\", ATTR{queue/rotational}==\"1\", ATTR{queue/scheduler}=\"$scheduler\", ATTR{queue/rq_affinity}=\"2\", ATTR{queue/read_ahead_kb}=\"2048\", ATTR{queue/nr_requests}=\"1024\"" >> $path
+    
+elif [ "$scheduler" == "cfq" ]; then
+    read -p 'SSD or HDD?: ' type
+
+    if [ "$type" == "SSD" ]; then
+    
+        echo $scheduler > /sys/block/$disk/queue/scheduler
+        cat /sys/block/$disk/queue/scheduler
+        sed -i -e '/^ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd\[a-z\]"/ s/^/#/;' $path
+        sed -i -e "/^ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\"/d" $path
+        sed -i "s/non-rotating disks/&\nACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\", ATTR{queue\/scheduler}=\"$scheduler\", ATTR{queue\/read_ahead_kb}=\"0\"/" $path
+    elif [ "$type" == "HDD" ]; then
+    
+        echo $scheduler > /sys/block/$disk/queue/scheduler
+        cat /sys/block/$disk/queue/scheduler
+        sed -i -e '/^ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd\[a-z\]"/ s/^/#/;' $path
+        sed -i -e "/^ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\"/d" $path
+        echo "ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNEL==\"$disk\", ATTR{queue/rotational}==\"1\", ATTR{queue\/scheduler}=\"$scheduler\", ATTR{queue/rq_affinity}=\"2\, ATTR{queue/read_ahead_kb}=\"2048\", ATTR{queue/nr_requests}=\"1024\"" >> $path
+    else
+        echo "There is no type"
+    fi
+    
+else 
+    echo "Value is wrong"
+fi
+echo
+cat $path
