507d7a441519cc28d488197967830e37a63947c5
root <feshchenko.vitalii@gmail.com>
2023-03-21 23:34:51 -0400

    started implementation of functions for datastores and vSwitch objects

--- functions_validation.ps1
+++ functions_validation.ps1
@@ -1,3 +1,4 @@
+# Connecting to VSA VM and get VM settings data
 function VSA-VM {
     param($vmName)
     $vm = Get-VM -Name $vmname
@@ -7,5 +8,35 @@ function VSA-VM {
         Write-Host "Sockets: $($vm.ExtensionData.Config.Hardware.NumCPU / $vm.ExtensionData.Config.Hardware.NumCoresPerSocket)"
         Write-Host "RAM: $($vm.MemoryGB)"
         Write-Host "Version: $($vm.HardwareVersion)"
-    }
+        $nics = Get-NetworkAdapter -VM $vm
+        foreach ($nic in $nics) {
+            Write-Host "    Network name: " -nonewline; Write-host "$($nic.NetworkName), $($nic.type)" -f red
+        }
+
+    } else {
+        Write-Host "VM is not found"
+      }
 }
+
+# Creating vswitch objects in case I need to manipulate their data
+
+function VSA-HostSwitchObject {
+    $switches = Get-VirtualSwitch | Select-Object Name, Nic, MTU
+    $switchObjects = @()
+    foreach ($switch in $switches) {
+        $switchObject = New-Object -TypeName psobject
+        $switchObject | Add-Member -MemberType NoteProperty -Name Name -Value $switch.Name
+        $switchObject | Add-Member -MemberType NoteProperty -Name Nic -Value $switch.Nic
+        $switchObject | Add-Member -MemberType NoteProperty -Name MTU -Value $switch.MTU
+        $switchObjects += $switchObject
+    return $switchObjects
+}
+}
+
+function VSA-HostDatastore {
+    param($ESXiHost)
+    $datastores = Get-Datastore -VMHost $ESXiHost
+    return $datastores
+}
+
+
--- switches.ps1
+++ switches.ps1
@@ -1,6 +1,7 @@
 #!/usr/bin/pwsh
 
 Import-Module ./functions_validation.ps1
+
 $env:PSModulePath="/root/.local/share/powershell/Modules:/usr/local/share/powershell/Modules:/opt/microsoft/powershell/7/Modules"
 $env:HOME="/root"
 Set-PowerCLIConfiguration -InvalidCertificateAction ignore -Confirm:$false | Out-Null
@@ -18,55 +19,35 @@ $ESXiPassword2 = "Zxcvb12345!"
 Connect-VIServer $ESXiHost1 -User $ESXiUser1 -Password $ESXiPassword1 | Out-Null
 
 #Getting switch info via the command below
-$switchObjects_1 = Get-VirtualSwitch -VMHost $ESXiHost1 | Select-Object Name, Nic, MTU, VLanID, SwitchType
-
-#Creating switch objects in case I need to manipulate their data
-$switches1 = Get-VirtualSwitch | Select-Object Name, Nic, MTU
-$switchObjects1 = @()
-foreach ($switch in $switches1) {
-    $switchObject = New-Object -TypeName psobject
-    $switchObject | Add-Member -MemberType NoteProperty -Name Name -Value $switch.Name
-    $switchObject | Add-Member -MemberType NoteProperty -Name Nic -Value $switch.Nic
-    $switchObject | Add-Member -MemberType NoteProperty -Name MTU -Value $switch.MTU
-    $switchObjects1 += $switchObject
-}
+$switches_1 = Get-VirtualSwitch -VMHost $ESXiHost1 | Select-Object Name, Nic, MTU, VLanID, SwitchType
 
 #StarWind VM networks
 #Find a VSA VM on the host
 $vmName = "StarWindVSA_01_Feshchenko"
 VSA-VM "$vmName"
 
+# Get host datastore
+VSA-HostDatastore "$ESXiHost1"
 Disconnect-VIServer $ESXiHost1 -Confirm:$false
-
 
 
 #SECOND NODE###############################################
 Connect-VIServer $ESXiHost2 -User $ESXiUser2 -Password $ESXiPassword2 | Out-Null
 
 #Getting switch info via the command below
-$switchObjects_2 = Get-VirtualSwitch -VMHost $ESXiHost2 | Select-Object Name, Nic, MTU
-
-#Creating switch objects in case I need to manipulate their data
-$switches2 = Get-VirtualSwitch | Select-Object Name, Nic, MTU
-$switchObjects2 = @()
-foreach ($switch in $switches2) {
-    $switchObject = New-Object -TypeName psobject
-    $switchObject | Add-Member -MemberType NoteProperty -Name Name -Value $switch.Name
-    $switchObject | Add-Member -MemberType NoteProperty -Name Nic -Value $switch.Nic
-    $switchObject | Add-Member -MemberType NoteProperty -Name MTU -Value $switch.MTU
-    $switchObjects2 += $switchObject
-}
-
+$switches_2 = Get-VirtualSwitch -VMHost $ESXiHost2 | Select-Object Name, Nic, MTU
 
 
 Disconnect-VIServer $ESXiHost2 -Confirm:$false
 
 #### Disconnection from all nodes ####
 
-$table1 = $switchObjects_1 | Format-Table -AutoSize | Out-String
-$table2 = $switchObjects_2 | Format-Table -AutoSize | Out-String
+$table1 = $switches_1 | Format-Table -AutoSize | Out-String
+$table2 = $switches_2 | Format-Table -AutoSize | Out-String
 
 Write-Output $ESXiHost1
 Write-Output $table1 
 Write-Output $ESXiHost2
 Write-Output $table2
+
+
